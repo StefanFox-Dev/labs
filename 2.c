770c4bfff9ad76fe9d49989ebaf29297037f4929
StefanFox <59245729+StefanFox-Dev@users.noreply.github.com>
2025-09-29 16:51:46 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -5,15 +5,10 @@ int main() {
     double a, b, c, d;
 
     printf("Утворити прямокутник.\nВведіть чотири цифри: ");
-    
-    if (scanf("%lf %lf %lf %lf", &a, &b, &c, &d) != 4) {
-        printf("Немає чотирьох цифр.\n");
-        return 1;
-    }
+    scanf("%lf %lf %lf %lf", &a, &b, &c, &d);
 
     if (a == 0 || b == 0 || c == 0 || d == 0) {
         printf("Ділити на нуль не можна.\n");
-        return 1;
     } else {
         if ((a == b && c == d) || (a == c && b == d) || (a == d && b == c)) {
             printf("Співвідношення рівні.\n");
@@ -21,6 +16,5 @@ int main() {
             printf("Співвідношення не рівні.\n");
         }
     }
-
     return 0;
 }
