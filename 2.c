21318f359e198312d333e761fea1173ec2b114e0
StefanFox <59245729+StefanFox-Dev@users.noreply.github.com>
2025-09-29 16:28:24 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -4,21 +4,21 @@ int main() {
     
     double a, b, c, d;
 
-    printf("Enter four numbers: ");
+    printf("Утворити прямокутник.\nВведіть чотири цифри: ");
     
     if (scanf("%lf %lf %lf %lf", &a, &b, &c, &d) != 4) {
-        printf("Invalid input. Please enter four numeric values.\n");
+        printf("Немає чотирьох цифр.\n");
         return 1;
     }
 
     if (a == 0 || b == 0 || c == 0 || d == 0) {
-        printf("Error: Division by zero is not allowed.\n");
+        printf("Ділити на нуль не можна.\n");
         return 1;
     } else {
         if ((a == b && c == d) || (a == c && b == d) || (a == d && b == c)) {
-            printf("The ratios are equal.\n");
+            printf("Співвідношення рівні.\n");
         } else {
-            printf("The ratios are not equal.\n");
+            printf("Співвідношення не рівні.\n");
         }
     }
 
