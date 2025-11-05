ba99f27352a32ee99b8a2824dcfbaca7bac2e2d3
StefanFox <59245729+StefanFox-Dev@users.noreply.github.com>
2025-11-05 12:44:43 +0200

    Not use B

--- 4.c
+++ 4.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 
 void main() {
-    int i, n, k, A[200], B[200];
-    
-    printf("Введіть n, k: ");
-    scanf("%d%d", &n, &k);
+    int i, n, k, A[200];
+    
+    printf("Введіть n, k: ");
+    scanf("%d%d", &n, &k);
 
-    for (i = 0; i < n; i++) {
-        scanf("%d", &A[i]);
-    }
+    printf("Введіть %d елементів масиву: ", n);
+    for (i = 0; i < n; i++) scanf("%d", &A[i]);
 
-    k %= n;
+    k %= n;
 
-    for (i = 0; i < n; i++) {
-        B[(i + k) % n] = A[i];
-    }
+    if (k > 0) {
+        int count = 0;
+        
+        for (i = 0; count < n; i++) {
+            int ind = i;
+            int AA = A[i];
 
-    for (i = 0; i < n; i++) {
-        printf("%d ", B[i]);
-    }
-    printf("\n");
+            while (1) {
+                int indN = (ind - k + n) % n; 
+                count++;
+                
+                if (indN == i) {
+                    A[ind] = AA;
+                    break;
+                }
+                
+                A[ind] = A[indN];
+                ind = indN;
+            }
+        }
+    }
+
+    printf("\nРезультат зсуву: ");
+    for (i = 0; i < n; i++) printf("%d ", A[i]);
 }
